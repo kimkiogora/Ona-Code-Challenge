474e554c78e2fc691a30aedd57c29cefb2448d66
Kim Kiogora <kimkiogora@gmail.com>
2015-06-04 12:01:13 +0300

    Create Algorithms.php

---
+++ src/Lib/Algorithms.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * This class holds the algorithms.
+ *
+ * @category  ONA
+ * @package   Lib
+ * @author    Kim Kiogora <kimkiogora@gmail.com>
+ * @copyright 2015 ONA Code Challenge
+ * @license   Proprietory License
+ * @version   Release:1.0.0
+ * @link      http://github.com/kimkiogora/Ona-Code-Challenge
+ */
+class Algorithms {
+
+    /**
+     * Calculate the functional water points.
+     * 
+     * @param type $dataset
+     */
+    public static function Calculate($dataset) {
+        $counts = array();
+        $community_array = array();//water points per community
+        $community_arrayx = array();//non-functional water points
+        $ranking_array = array();//ranking  by percentage
+
+        $counter = 0;
+        
+        foreach ($dataset as $data => $value) {
+            $stat = $value['water_functioning'];
+            $community = $value['communities_villages'];
+
+            if (!in_array($community, $community_array)) {
+                $community_array[$community] = Algorithms::getWaterPointsToTal($dataset, $community);
+                $community_arrayx[$community] = Algorithms::getWaterPointsToTal($dataset, $community,'no');
+                $ranking_array=$community_arrayx;
+            }
+
+            if (strcasecmp($stat, "yes") == 0) {
+                ++$counter;
+            }
+        }
+
+        $counts['functional_water_points'] = $counter;
+        $counts['number_of_water_points'] = json_encode($community_array);
+        
+        //Ranking will be a merger
+        $counts['community_ranking'] = Algorithms::getRanking($ranking_array);
+
+        return $counts;
+    }
+
+    /**
+     * Count the number of water points per community.
+     * @param type $dataset
+     * @param type $community
+     */
+    private static function getWaterPointsToTal($dataset, $community,
+            $functioning = 'yes') {
+        $counter = 0;
+        foreach ($dataset as $data => $value) {
+            $community_cmp = $value['communities_villages'];
+            if (strcasecmp($community, $community_cmp) == 0) {
+                if ($functioning == NULL || empty($functioning)) {
+                    ++$counter;
+                } else {
+                    $stat = $value['water_functioning'];
+                    if (strcasecmp($functioning, $stat) == 0) {
+                        ++$counter;
+                    }
+                }
+            }
+        }
+        return $counter;
+    }
+
+    /**
+     * Count the number of water points per community.
+     * @param type $dataset
+     * @param type $community
+     */
+    private static function getRanking($data) {
+        $values = array();
+        $total=0;
+        foreach ($data as $community=>$broken_water_points){
+            $total=$total+$broken_water_points;
+        }  
+        
+        //do a percentage
+        $perc = array();
+        foreach ($data as $community=>$broken_water_points){
+            $p = ($broken_water_points*100)/$total;
+            $perc[$community]=  sprintf("%.2f", $p);
+        }  
+        
+        array_multisort($perc);
+        
+        return json_encode($perc);
+    }
+
+    /**
+     * Function sends a GET request.
+     *  
+     * @param type $url
+     * @return string
+     */
+    public static function getDataSet($url) {
+        $ch = curl_init();
+
+        curl_setopt_array($ch, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => "ONA Code Challenge"));
+
+        $resp = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $resp;
+    }
+
+}
