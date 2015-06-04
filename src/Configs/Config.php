11f3a940c4ac5aad625f217c2e9629eabf76baad
Kim Kiogora <kimkiogora@gmail.com>
2015-06-04 12:03:40 +0300

    Create Config.php

---
+++ src/Configs/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This class holds the configuration settings.
+ *
+ * @category  ONA
+ * @package   Lib
+ * @author    Kim Kiogora <kimkiogora@gmail.com>
+ * @copyright 2015 ONA Code Challenge
+ * @license   Proprietory License
+ * @version   Release:1.0.0
+ * @link      http://github.com/kimkiogora/OCC
+ */
+class Config {
+    
+    const URL = "https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json";
+    const IS_FUNCTIONAL="yes";
+    const INVALID_RESPONSE_FORMAT="INVALID REPONSE FORMAT";
+}
