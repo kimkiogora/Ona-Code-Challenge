adff9f56cff151de05a36539d0ec8c84b8f6effa
Kim Kiogora <kimkiogora@gmail.com>
2015-06-04 13:09:49 +0300

    Update run.php

--- src/run.php
+++ src/run.php
@@ -15,19 +15,29 @@ require_once 'Configs/Config.php';
 require_once 'Lib/Algorithms.php';
 
 $start_time = microtime(true);
+
+//Retrieve the dataset from given URL
 $dataset = Algorithms::getDataSet(Config::URL);
 
 if (json_decode($dataset, TRUE)) {
     $decoded_set = json_decode($dataset, TRUE);
 
+    //Perform the required calculations
     $calc 
             = Algorithms::Calculate($decoded_set);
     $tat = microtime(true)-$start_time;
     
+    //TAT is useful for redesign of slow algorithms
     $calc['TAT'] = "$tat sec";
 
+    //Display the response in array format 
     print_r($calc);
+    
+    //Or display the response in json format 
+    //echo json_encode($calc);
 } else {
+    
+    //return an invalid response message
     die(Config::INVALID_RESPONSE_FORMAT);
 }
 
